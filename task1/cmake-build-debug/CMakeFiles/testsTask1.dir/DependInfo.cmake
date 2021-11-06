e1b39fada9b82e8ec7c0b23dee6cdba757ab5cde
bralexx <bruek.aiu@phystech.edu>
2021-11-06 20:12:09 +0300

    task1

---
+++ task1/cmake-build-debug/CMakeFiles/testsTask1.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "D:/GitHub/FormalLanguagesPracticalTasks/task1/solve.cpp" "D:/GitHub/FormalLanguagesPracticalTasks/task1/cmake-build-debug/CMakeFiles/testsTask1.dir/solve.cpp.obj"
+  "D:/GitHub/FormalLanguagesPracticalTasks/task1/test_main.cpp" "D:/GitHub/FormalLanguagesPracticalTasks/task1/cmake-build-debug/CMakeFiles/testsTask1.dir/test_main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
