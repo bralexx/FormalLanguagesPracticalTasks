344f1b1fe28069fd1c322272ccd0099b03afb6ae
bralexx <bruek.aiu@phystech.edu>
2021-11-06 20:17:38 +0300

    init

---
+++ earleyAlgo/cmake-build-debug/CMakeFiles/earleyAlgo.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/earleyAlgo.dir/main.cpp.obj"
+  "earleyAlgo.exe"
+  "earleyAlgo.exe.manifest"
+  "earleyAlgo.pdb"
+  "libearleyAlgo.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/earleyAlgo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
